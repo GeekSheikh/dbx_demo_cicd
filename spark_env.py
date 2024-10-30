7b7135840b41796aa308d8e5f9749a2e005ad30d
geeksheikh <geeksheikh@users.noreply.github.com>
2024-10-30 16:24:57 -0400

    initial building blocks enabled

---
+++ spark_env.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from pyspark.sql import SparkSession
+from databricks.connect import DatabricksSession
+from databricks.sdk.core import Config
+
+# load_dotenv()
+
+## Set the environment variable IS_LOCAL_ENV to "true" if running on local environment
+## In VSCode, I do this settings --> terminal.integrated.env --> edit json --> add env vars here
+if bool(os.environ.get("IS_LOCAL_ENV")):
+    print("Driver is running on local environment")
+    
+    ## Proxy settings / env vars go here
+    
+    config = Config(
+        profile = "field-eng",
+        cluster_id = "1030-174504-b6mprahm"
+    )
+    spark = DatabricksSession.builder.sdkConfig(config).getOrCreate()
+else:
+    print("Driver is running on Databricks")
+    spark = SparkSession.builder.appName("dbx_demo_cicd").getOrCreate()
+    
